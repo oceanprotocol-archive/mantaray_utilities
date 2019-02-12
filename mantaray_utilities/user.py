26d7303d73c7cd3e5cd8febb4356fc6c699bbf09
MarcusJones <jones.0bj3@gmail.com>
2019-02-12 11:46:03 +0100

    update password function

--- mantaray_utilities/user.py
+++ mantaray_utilities/user.py
@@ -12,19 +12,26 @@ PATH_CONFIG = get_config_file_path()
 import csv
 import os
 
-def password_map(acct, password_dict):
-    return "asdf"
+def password_map(address, password_dict):
+    if str.lower(address) in password_dict:
+        password = password_dict[str.lower(address)]
+        return password
+    else:
+        return False
 
 def load_passwords(path_passwords):
-    # %%
-    # Get all passwords
+    # Get passwords from account, password CSV file
     assert os.path.exists(path_passwords)
     passwords = dict()
-    # path_passwords = get_project_path() / 'passwords.csv'
     with open(path_passwords) as f:
         for row in csv.reader(f):
             if row:
                 passwords[row[0]] = row[1]
 
     passwords = {k.lower(): v for k, v in passwords.items()}
+    logging.info("{} account-password pairs loaded".format(len(passwords)))
     return passwords
+
+def get_password(path_passwords, account):
+    passwords = load_passwords(path_passwords)
+
