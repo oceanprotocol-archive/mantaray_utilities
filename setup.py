7c14c5a5a034e19da85556338295e507cee03eef
MarcusJones <jones.0bj3@gmail.com>
2019-07-10 12:07:51 +0200

    Bump version: 0.2.2 → 0.2.3

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ print()
 setup(
     name='mantaray_utilities',
     author="leucothia",
-    version='0.2.2',
+    version='0.2.3',
     author_email='devops@oceanprotocol.com',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
